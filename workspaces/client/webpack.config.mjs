13cf87baced08db7749039e6a4a2eae4c7b3e313
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-01-03 16:16:27 +0900

    Change devtool as eval

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -5,7 +5,7 @@ import webpack from 'webpack';
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  devtool: 'source-map',
+  devtool: 'eval',
   entry: './src/main.tsx',
   mode: 'none',
   module: {
